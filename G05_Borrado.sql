ee13a96d189e523e2dea6965bb29438225df2485
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-06-20 10:46:57 -0300

    script de borrado de tablas - borrado.sql

--- G05_Borrado.sql
+++ G05_Borrado.sql
@@ -1,1 +1,12 @@
 SET search_path = unc_249695;
+
+DROP TABLE IF EXISTS gr05_patrocinios;
+DROP TABLE IF EXISTS gr05_patrocinante;
+DROP TABLE IF EXISTS gr05_evento_edicion;
+DROP TABLE IF EXISTS gr05_evento;
+DROP TABLE IF EXISTS gr05_distrito;
+DROP TABLE IF EXISTS gr05_provincia;
+DROP TABLE IF EXISTS gr05_pais;
+DROP TABLE IF EXISTS gr05_usuario;
+DROP TABLE IF EXISTS gr05_subcategoria;
+DROP TABLE IF EXISTS gr05_categoria;
