3a252cfa5cad8e96c4ea5dbbfaaf2b9e1caddc66
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-06-20 18:17:19 -0300

    implementacion restriccion de tupla a)

--- G05_Cambios.sql
+++ G05_Cambios.sql
@@ -1,1 +1,9 @@
 SET search_path = unc_249695;
+
+--a)Se debe consistir que la fecha de inicio de la publicación de la edición
+--sea anterior a la fecha de fin de la publicación del mismo si esta última no es nula.
+--Restricción de tupla
+ALTER TABLE gr05_evento_edicion
+ADD CONSTRAINT ck_inicio_fin_publicacion
+CHECK ( ( fecha_fin_pub IS NOT NULL AND fecha_fin_pub > fecha_inicio_pub )
+	OR fecha_fin_pub IS NULL);
