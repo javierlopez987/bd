ea2bc8e3ba8cf6aaed3bf664a5c409a8c5403b5c
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-06-22 19:15:10 -0300

    inserción de más datos para testing

--- tpe_project.sql
+++ tpe_project.sql
@@ -8,10 +8,12 @@ INSERT INTO gr05_pais VALUES ('Argentina');
 --Tabla PROVINCIA
 INSERT INTO gr05_provincia VALUES ('Argentina', 'Buenos Aires');
 INSERT INTO gr05_provincia VALUES ('Argentina', 'San Juan');
+INSERT INTO gr05_provincia VALUES ('Argentina', 'Río Negro');
 
 --Tabla DISTRITO
 INSERT INTO gr05_distrito VALUES (1, 'Argentina', 'Buenos Aires', 'Tandil');
 INSERT INTO gr05_distrito VALUES (2, 'Argentina', 'San Juan', 'San Juan');
+INSERT INTO gr05_distrito VALUES (3, 'Argentina', 'Río Negro', 'Bariloche');
 
 --Tabla USUARIO
 INSERT INTO gr05_usuario VALUES (1, 'Javier', 'Lopez Ferreira', 'jlopezferreira@alumnos.exa.unicen.edu.ar', '1234');
@@ -21,9 +23,15 @@ INSERT INTO gr05_categoria VALUES (1, 'Deportivo');
 
 --Tabla SUBCATEGORIA
 INSERT INTO gr05_subcategoria VALUES (1, 1, 'Trekking');
+INSERT INTO gr05_subcategoria VALUES (1, 2, 'Escalada');
+INSERT INTO gr05_subcategoria VALUES (1, 3, 'Running');
+INSERT INTO gr05_subcategoria VALUES (1, 4, 'Triatlón');
+INSERT INTO gr05_subcategoria VALUES (1, 5, 'Ski');
 
 --Tabla EVENTO
 INSERT INTO gr05_evento VALUES (1, 'Aventura Las Ánimas', 'Trekking de aventura por el Cerro Las Ánimas y alrededores', 1, 1, 1, 1, 21,9,true);
+INSERT INTO gr05_evento VALUES (2, 'Hombre de Hierro', 'Carrera de triatlón en el Lago del Fuerte y alrededores', 1, 4, 1, 1, 20,11,true);
+INSERT INTO gr05_evento VALUES (3, 'Ski Syncro', 'Competencia de ski sincronizado en el Cerro Catedral', 1, 5, 1, 3, 10,8,true);
 
 --Tabla EVENTO_EDICION
 INSERT INTO gr05_evento_edicion VALUES (1, 1, to_date('21/06/2020','DD/MM/YYYY'), NULL, 60000.00, NULL);
