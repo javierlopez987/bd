c504c957d5559f9ad7793ef03799c58dc1c11268
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-06-22 22:14:16 -0300

    implementacion de vista #3

--- G05_Cambios.sql
+++ G05_Cambios.sql
@@ -24,3 +24,18 @@ CREATE VIEW cant_eventos_distrito AS
     JOIN gr05_distrito d ON (e.id_distrito = d.id_distrito)
     GROUP BY nombre_distrito, nombre_provincia, nombre_pais;
 
+--3)Datos Categorías que poseen eventos en todas sus subcategorías
+CREATE VIEW categoria_all_subcategorias_con_eventos AS
+SELECT *
+FROM gr05_categoria c
+WHERE NOT EXISTS(
+    SELECT 1
+    FROM gr05_subcategoria s
+    WHERE (c.id_categoria = s.id_categoria)
+        AND NOT EXISTS(
+            SELECT id_categoria, id_subcategoria
+            FROM gr05_evento e
+            WHERE (s.id_categoria = e.id_categoria
+                AND s.id_subcategoria = e.id_subcategoria)
+        )
+    );
