903ad75b8e505f5fadb8219e89b1e3a1da125932
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-06-27 14:34:45 -0300

    se agregan datos de comprobación manual

--- scripts/G05_Datos.sql
+++ scripts/G05_Datos.sql
@@ -6,11 +6,13 @@ INSERT INTO gr05_pais VALUES ('Argentina');
 INSERT INTO gr05_provincia VALUES ('Argentina', 'Buenos Aires');
 INSERT INTO gr05_provincia VALUES ('Argentina', 'San Juan');
 INSERT INTO gr05_provincia VALUES ('Argentina', 'Río Negro');
+INSERT INTO gr05_provincia VALUES ('Argentina', 'Ciudad Autónoma de Buenos Aires');
 
 --Tabla DISTRITO
 INSERT INTO gr05_distrito VALUES (1, 'Argentina', 'Buenos Aires', 'Tandil');
 INSERT INTO gr05_distrito VALUES (2, 'Argentina', 'San Juan', 'San Juan');
 INSERT INTO gr05_distrito VALUES (3, 'Argentina', 'Río Negro', 'Bariloche');
+INSERT INTO gr05_distrito VALUES (4, 'Argentina', 'Ciudad Autónoma de Buenos Aires', 'Ciudad Autónoma de Buenos Aires');
 
 --Tabla USUARIO
 INSERT INTO gr05_usuario VALUES (1, 'Javier', 'Lopez Ferreira', 'jlopezferreira@alumnos.exa.unicen.edu.ar', '1234');
@@ -33,11 +35,17 @@ INSERT INTO gr05_evento VALUES (3, 'Ski Syncro', 'Competencia de ski sincronizad
 
 --Tabla EVENTO_EDICION
 INSERT INTO gr05_evento_edicion VALUES (1, 1, to_date('21/06/2020','DD/MM/YYYY'), NULL, 60000.00, NULL);
-
+INSERT INTO gr05_evento_edicion VALUES (3, 1, to_date('01/07/2020','DD/MM/YYYY'), NULL, 200000.00, NULL);
 --Tabla PATROCINANTE
 INSERT INTO gr05_patrocinante VALUES (1, 'Ansilta', 'Juan', 'Pérez', 'Necochea 2085', 2);
 INSERT INTO gr05_patrocinante VALUES (2, 'Centro de Montaña Tandil', 'Juliana', 'Gómez', 'Pje El Centinela S/N', 1);
-
+INSERT INTO gr05_patrocinante VALUES (3, 'Northface', 'Pedro', 'González', 'Av. Santa Fe 3253', 4);
+INSERT INTO gr05_patrocinante VALUES (4, 'Naka Outdoors', 'Ramiro', 'Nakarato', 'Ramallo 2307', 4);
+INSERT INTO gr05_patrocinante VALUES (5, 'Head Argentina', 'Esteban', 'Headache', NULL, 4);
 --Tabla PATROCINIOS
 INSERT INTO gr05_patrocinios VALUES (1,1,1,58000.00);
 INSERT INTO gr05_patrocinios VALUES (2,1,1,2000.00);
+INSERT INTO gr05_patrocinios VALUES (3,3,1,80000.00);
+INSERT INTO gr05_patrocinios VALUES (4,3,1,20000.00);
+INSERT INTO gr05_patrocinios VALUES (1,3,1,80000.00);
+INSERT INTO gr05_patrocinios VALUES (5,3,1,60000.00);
